dd2c3a10bd2c872decdfbf9767fcaa907bcd7430
Arthur Klein <kleinarthu@cy-tech.fr>
2024-01-31 10:03:56 +0100

    Changed the returned value when error in the api

--- api.py
+++ api.py
@@ -21,7 +21,7 @@ def get_gps_data(ip: str):
     cursor.execute("SELECT ip, latitude, longitude, timestamp_ FROM gps_data WHERE ip = %s ORDER BY timestamp_ DESC LIMIT 1", (ip,))
     row = cursor.fetchone()
     if row == None:
-        return None
+        return {}
     return GPSData(ip=row[0], latitude=row[1], longitude=row[2], timestamp=row[3])
 
 uvicorn.run(app, host="0.0.0.0", port = 8080)
